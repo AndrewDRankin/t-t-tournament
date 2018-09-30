39f9e9f22360566cc072fd49dade78417b31034a
Andy Rankin <wave@andrewdrankin.com>
2018-09-30 11:20:25 +0100

    Forced even player count in players array

--- src/components/TournamentBracket.jsx
+++ src/components/TournamentBracket.jsx
@@ -1,20 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 // import { Link } from 'react-router-dom';
 
 
-const TournamentBracket = ({names}) => {
-    return (
-        <Fragment>
-            <p>
-                Tournament Time!
-            </p>
-            <p>
-                {names}
-            </p>
-            {/* <Link to={'/'} className='btn btn-primary'>Start again!</Link> */}
-        </Fragment>
-    )
+class TournamentBracket extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: [],
+            history: [],
+        };
+    }
+    
+    render() {
+        this.props.names.map((player, index) => {
+            return this.state.players.push(player)
+        });
+        let players = this.state.players;
+        players.length % 2 !== 0 ? players.push("No opponent") : null; 
+        console.log(players);
+        return (
+            <Fragment>
+                <p>
+                    Output
+                </p>
+            </Fragment>
+        );
+    }
 };
+
+
+
+
+
+
+
+// const TournamentBracket = ({names}) => {
+//     return (
+//         <Fragment>
+//             <p>
+//                 Tournament Time!
+//             </p>
+//             <p>
+//                 {names}
+//             </p>
+//             {/* <Link to={'/'} className='btn btn-primary'>Start again!</Link> */}
+//         </Fragment>
+//     )
+// };
 
 
 export default TournamentBracket;
