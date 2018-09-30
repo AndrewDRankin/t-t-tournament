71b0d22ec194450690a69f3f7a4e4874b9b5dea4
Andy Rankin <wave@andrewdrankin.com>
2018-09-30 14:50:17 +0100

    Experimenting with history array

--- src/components/TournamentBracket.jsx
+++ src/components/TournamentBracket.jsx
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-// import { Link } from 'react-router-dom';
 
 
 class TournamentBracket extends Component {
@@ -9,44 +8,56 @@ class TournamentBracket extends Component {
             players: [],
             history: [],
         };
+        this.pairPicker = this.pairPicker.bind(this);
     }
-    
+    pairPicker() {
+        let players = this.state.players;
+        let pick = [];
+        for (let i = 1; i <= players.length/2; i += 1) {
+            pick.push(
+                <p key={i}>
+                    {
+                        playerOne = players[Math.floor(Math.random() *players.length)]
+                        while (history.indexOf(playerOne) !== -1) {
+                            playerOne = players[Math.floor(Math.random() *players.length)]
+                        };
+                        history.push(playerOne);
+                        return playerOne;
+                    } - {
+                        playerTwo = players[Math.floor(Math.random() *players.length)]
+                        while (history.indexOf(playerTwo) !== -1) {
+                            playerTwo = players[Math.floor(Math.random() *players.length)]
+                        };
+                        history.push(playerTwo);
+                        return playerTwo;
+                    }
+                </p>
+            )
+        }
+        return pick;
+    };
     render() {
         this.props.names.map((player, index) => {
             return this.state.players.push(player)
         });
         let players = this.state.players;
-        players.length % 2 !== 0 ? players.push("No opponent") : null; 
+        players.length % 2 !== 0 ? players.push("Default to next round") : null; 
         console.log(players);
         return (
             <Fragment>
-                <p>
-                    Output
-                </p>
+                <div>
+                    Pairings
+                    {this.pairPicker()}
+                </div>
             </Fragment>
         );
     }
 };
 
 
+// {history.push(player)}
 
-
-
-
-
-// const TournamentBracket = ({names}) => {
-//     return (
-//         <Fragment>
-//             <p>
-//                 Tournament Time!
-//             </p>
-//             <p>
-//                 {names}
-//             </p>
-//             {/* <Link to={'/'} className='btn btn-primary'>Start again!</Link> */}
-//         </Fragment>
-//     )
-// };
+// if index !== players.index
 
 
 export default TournamentBracket;
